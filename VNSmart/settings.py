50bf65f06be51957d29c13c841e6738e9f67ee8d
Bubudiep <hlitdvp98@gmail.com>
2024-02-16 16:36:43 +0700

    update setting database

--- VNSmart/settings.py
+++ VNSmart/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'vnsmart',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Pan123456!!',
         'HOST': 'localhost',
         'PORT': '3306',
     }
